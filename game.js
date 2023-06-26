cf6ff6e003291853f51921c314bf63d3d8b001cc
Jeffrey D. McGovern <jeffreydmcgovern@gmail.com>
2023-06-25 19:22:35 -0700

    fix a typo that crashed the game

--- game.js
+++ game.js
@@ -32,7 +32,7 @@ function formatResult(result) {
   const { win, lose, verb } = result;
   return verb === "tie"
     ? `<@${win.id}> and <@${lose.id}> draw with **${win.objectName}**`
-    : `<@${win.id}>'s **${win.objectName}** ${verb} <@${lost.id}>'s **${lose.objectName}**`;
+    : `<@${win.id}>'s **${win.objectName}** ${verb} <@${lose.id}>'s **${lose.objectName}**`;
 }
 
 // thie is just to figure out winner + verb
